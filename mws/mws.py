c6c3ae8fc12bf4165e2b269146222e37fff3b424
Stefan Hospes <shospes@gmail.com>
2016-10-06 14:40:46 +0200

    Added marketplace IDS

--- mws/mws.py
+++ mws/mws.py
@@ -55,6 +55,20 @@ MARKETPLACES = {
     "MX": "https://mws.amazonservices.com.mx",     # A1AM78C64UM0Y8
 }
 
+MARKETPLACE_IDS = {
+    "CA": "A2EUQ1WTGCTBG2",
+    "US": "ATVPDKIKX0DER",
+    "DE": "A1PA6795UKMFR9",
+    "ES": "A1RKKUPIHCS9HS",
+    "FR": "A13V1IB3VIYZZH",
+    "IN": "A21TJRUUN4KGV",
+    "IT": "APJ6JRA9NG5V4",
+    "UK": "A1F83G8C2ARO7P",
+    "JP": "A1VC38T7YXB528",
+    "CN": "AAHKV2X7AFYLW",
+    "MX": "A1AM78C64UM0Y8",
+}
+
 
 class MWSError(Exception):
     """
